1a24b9d20c2ff8585428cf86df1da239999db3f7
parkdabeen <elqla19@gmail.com>
2023-01-26 18:10:12 +0900

    test

--- NomardWeather/App.js
+++ NomardWeather/App.js
@@ -1,14 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={ {
-        fontSize: 28,
-        color: 'red'
-        }}>Hello!</Text>
-      <StatusBar style="auto" />
+      <Text
+        style={{
+          fontSize: 28,
+          color: "red",
+        }}
+      >
+        Hello!
+      </Text>
+      {/* 시계, 배터리 등 상단 /style-auto../*/}
+      <StatusBar style="dark" />
     </View>
   );
 }
@@ -16,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
