17886ebc73f599d60270b42cc34f85442c8ba55e
dabeen <97648343+elqla@users.noreply.github.com>
2023-06-20 21:50:10 +0900

    toonflix

---
+++ myfirstandroidapp/app/src/main/java/com/pdb/myfirstandroidapp/Bts4Acitivity.kt
@@ -0,0 +1,11 @@
+package com.pdb.myfirstandroidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Bts4Acitivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bts4_acitivity)
+    }
+}
